58b3c1528e716f5c97f58c7a16db85e69eb45293
Juliann Talkington <perrigoj@bellsouth.net>
2019-06-05 09:48:55 -0700

    very basic pseudo to help define areas for meister

---
+++ 6_imhotep/gameState.js
@@ -0,0 +1,19 @@
+//create a class called GameState (data and values related to game)
+//constructor
+  //properties (variables)
+    //shipDocked (ship is docked) - boolean
+    //shipFull (ship is full) - boolean
+
+//methods
+  //moveship(color) method -- parameter of color from player.js
+    //resetRound method
+    //move block to the obelisk total for that player
+  //allocate points
+  //change turns
+    //if the round is not over
+      //move to the other player
+  //resetRound 
+    //if the ship is docked
+      //move from current player to other player
+      //undock the ship
+
