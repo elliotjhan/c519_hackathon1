299160c90f72ff6a6228893ee1bea5988651e52e
Elliot Han <elliotjhan@gmail.com>
2019-06-05 18:13:28 -0700

    cleaned up gamestate and player object classes

--- 6_imhotep/gameState.js
+++ 6_imhotep/gameState.js
@@ -1,11 +1,3 @@
-//create a class called GameState (data and values related to game)
-//constructor
-  //properties (variables)
-    //shipDocked (ship is docked) - boolean
-    //shipFull (ship is full) - boolean
-    //turn/ round trackers
-
-
 
 class GameState {
   constructor() {
@@ -13,19 +5,9 @@ class GameState {
     this.shipFull = false;
     this.playerTurn = 0;
     this.players = [];
+    this.round = 0;
   }
-
-  //methods
-  //moveship(color) method -- parameter of color from player.js
-    //resetRound method
-    //increase the obelisk total by one for that player
-  // moveShip() {
-  //   if(this.turn){
-  //   this.resetRound();
-  //   this.players[i]
-  // }
-
-  //createPlayer
+ 
   createPlayer(){
     var colorArray = ['brown', 'black'];
     for (var i = 0; i < colorArray.length; i++){
@@ -34,86 +16,67 @@ class GameState {
     }
   }
 
-  loadShip(){//player action1
-    //check if ship has empty space
-    //if yes
-      //increment ship counter
-      //decrement blocks
-      //set isTurn to false
-    //if no
-      //display message - no space in ship
+  loadShip(){
 
-    if(//ship is full){
+    if(this.shipFull) {
       alert('Ship is already full, please choose another action.');
-    }else{
-      this.blockcount--;
-
-      //fill space on ship
-      //increment blocksOnShip,
-      //assign color
-
-      this.isTurn = false;
+    } else {
+      this.players[this.playerTurn].blockCount--;
+      this.updateTurn();
+      this.shipFull = true;
     }
  }
 
- //player action2  
  getBlocks() {
 
    var blocks = this.players[this.playerTurn].blockCount;
-
-   if(blocks < 5){
-    blocks += 3;
+   if (blocks < 5) {
+     blocks += 3;
      if (blocks > 5) {
-      blocks = 5;
+     blocks = 5;
      }
-    this.updateTurn();
-
-   }else{
+     this.updateTurn();
+    } else {
      alert('Sled is already full, please choose another action.');
      return;
-   }
+    }
  }
- //player action3
-     //sailShip (moving the ship to the game board)
-    //call GameState moveShip(this.color) method -- moves the ship
-    //set isTurn to false
 
   sailShip() {
-    if(//ship is docked){
+    if(this.shipDocked){
       alert('Ship is already docked. Please pick another action.');
       return;
     //ship is not full)
-    } else if(//ship is not loaded){ 
+    } else if (!this.shipFull) {//ship is not loaded){ 
       alert('Ship is not loaded, please pick another action.');
       return;
-    }else{
-      //call gameState move ship, pass player color through
+    } else {
       alert('Ship has sailed! Blocks unloaded. Starting a new round.')
-    }    
-    this.updateTurn();
-  }
-
-  //allocate points
-  allocatePoints(){
-
-  }
-  //change turns
-    //if the round is not over
-      //move to the other player
-  changeTurns(color) {
-
-  }  
-  
-  //resetRound 
-    //if the ship is docked
-      //move from current player to other player
-      //undock the ship
-  resetRound() {
       this.updateTurn();
+      this.players[this.playerTurn].obeliskTotal += 1;
+      this.shipFull = false;
+      this.shipDocked = true;
+    }    
   }
 
-  updateObeliskTotal () {
+  allocatePoints(){
+    var p1Total = this.players[0].obeliskTotal;
+    var p2Total = this.players[1].obeliskTotal;
+  
+    if(p1Total > p2Total) {
+      this.players[0].score += 10;
+      this.players[1].score += 1;
+    } else {
+      this.players[1].score += 10;
+      this.players[0].score += 1;
+    }
+  }
 
+  resetRound() {
+    this.shipDocked = false;
+    if(this.round === 6) {
+      this.allocatePoints();
+    }
   }
 
   updateTurn() {
@@ -124,3 +87,34 @@ class GameState {
     }
   }
 }
+
+//create a class called GameState (data and values related to game)
+//constructor
+  //properties (variables)
+    //shipDocked (ship is docked) - boolean
+    //shipFull (ship is full) - boolean
+    //turn/ round trackers
+
+     //methods
+  //moveship(color) method -- parameter of color from player.js
+    //resetRound method
+    //increase the obelisk total by one for that player
+  // moveShip() {
+  //   if(this.turn){
+  //   this.resetRound();
+  //   this.players[i]
+  // }
+
+  //createPlayer
+
+   //player action1
+    //check if ship has empty space
+    //if yes
+      //increment ship counter
+      //decrement blocks
+      //set isTurn to false
+    //if no
+      //display message - no space in ship
+       //fill space on ship
+      //increment blocksOnShip,
+      //assign color
