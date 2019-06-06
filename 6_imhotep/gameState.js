137cd606a2905d59f6499fbf83e9f3e72f9be024
Juliann Talkington <perrigoj@bellsouth.net>
2019-06-05 21:39:42 -0700

    displaying numbers to display

--- 6_imhotep/gameState.js
+++ 6_imhotep/gameState.js
@@ -5,7 +5,7 @@ class GameState {
     this.shipFull = false;
     this.playerTurn = 0;
     this.players = [];
-    this.round = 0;
+    this.round = 1;
 
     this.getBlocks = this.getBlocks.bind(this);
     this.sailShip = this.sailShip.bind(this);
@@ -32,18 +32,17 @@ class GameState {
   }
 
   loadShip(){
-    
     if(this.shipFull) {
       alert('Ship is already full, please choose another action.');
     } else {
       this.players[this.playerTurn].blockCount -= 1;
+      $('.block-space').text('1/1');
       this.updateTurn();
       this.shipFull = true;
     }
  }
 
   getBlocks() {
-   
     if (this.players[this.playerTurn].blockCount < 5) {
       this.players[this.playerTurn].blockCount += 3;
 
@@ -61,7 +60,6 @@ class GameState {
   }
 
   sailShip() {
-    
     if(this.shipDocked){
       alert('Ship is already docked. Please pick another action.');
       return;
@@ -79,29 +77,44 @@ class GameState {
       this.players[this.playerTurn].obeliskTotal += 1;
       this.shipFull = false;
       this.shipDocked = true;
+      this.resetRound();
+      $('.block-space').text('0/1');
     }    
 
     console.log('this.players[this.playerTurn].obeliskTotal: ', this.players[this.playerTurn].obeliskTotal);
+    $('.stack-one').text(this.players[0].obeliskTotal);
+    $('.stack-two').text(this.players[1].obeliskTotal);
+    console.log('round', this.round);
   }
 
   allocatePoints(){
     var p1Total = this.players[0].obeliskTotal;
     var p2Total = this.players[1].obeliskTotal;
-  
     if(p1Total > p2Total) {
       this.players[0].score += 10;
       this.players[1].score += 1;
-    } else {
+    } else if (p1Total < p2Total){
       this.players[1].score += 10;
       this.players[0].score += 1;
+    } else {
+      this.players[1].score += 5;
+      this.players[0].score += 5;
     }
+    $('.round-tracker').text(this.round);
+    $('.score-one').text(this.players[0].score);
+    $('.score-two').text(this.players[1].score);
   }
 
   resetRound() {
+    $('.stack-one').text('0');
+    $('.stack-two').text('0');
+    this.round++;
     this.shipDocked = false;
-    if(this.round === 6) {
+    if(this.round > 6) {
       this.allocatePoints();
+      this.round = 1;
     }
+    $('.round-tracker').text(this.round);
   }
 
   updateTurn() {
@@ -126,36 +139,8 @@ class GameState {
       });
     }
   }
+  
+  resetState() {
 
+  }
 }
-
-//create a class called GameState (data and values related to game)
-//constructor
-  //properties (variables)
-    //shipDocked (ship is docked) - boolean
-    //shipFull (ship is full) - boolean
-    //turn/ round trackers
-
-     //methods
-  //moveship(color) method -- parameter of color from player.js
-    //resetRound method
-    //increase the obelisk total by one for that player
-  // moveShip() {
-  //   if(this.turn){
-  //   this.resetRound();
-  //   this.players[i]
-  // }
-
-  //createPlayer
-
-   //player action1
-    //check if ship has empty space
-    //if yes
-      //increment ship counter
-      //decrement blocks
-      //set isTurn to false
-    //if no
-      //display message - no space in ship
-       //fill space on ship
-      //increment blocksOnShip,
-      //assign color
--- 6_imhotep/player.js
+++ 6_imhotep/player.js
@@ -2,44 +2,8 @@
 class Player{
   constructor (color, startPosition) {
     this.score = null;
-    this.obeliskTotal = null;
+    this.obeliskTotal = 0;
     this.color = color;
     this.blockCount = null;
   }
 }
-
-
-// create a class called Player (data and values related to players)
-// constructor 
-  //properties (variables)
-    //player score
-    //obelisk total
-    //color
-    //blocks (number of pieces the player has at his or her disposal)
-    //isTurn (if it is the player's turn it will be true) - boolean, intialized to false
-    //bind the sailShip method
-
-//methods
-//loadTheShip 
-  //check if ship has empty space
-    //if yes
-      //increment ship counter
-      //decrement blocks
-      //set isTurn to false
-    //if no
-      //display message - no space in ship
-
-//getBlocks (user decides to get more blocks)
-  //add three to blocks
-  //call sled method
-
-//sled (controls the blocks in the sled)
-  //if blocks > 5
-    //if yes, blocks = 5
-  //set isTurn to false
-
-//sailShip (moving the ship to the game board)
-  //call GameState moveShip(this.color) method -- moves the ship
-  //set isTurn to false
-
-//
