6287e2e13a21de1eae6143fe853554861ad0625c
Juliann Talkington <37121177+redbearin@users.noreply.github.com>
2019-06-08 15:07:30 -0700

    Revert "Debugging"

--- 6_imhotep/main.js
+++ 6_imhotep/main.js
@@ -1,12 +1,16 @@
 $(document).ready(initApp);
 
-var game;
 
-//test
 function initApp() {
-  game = new GameState();
-
-  game.assignButtonHandlers();
-  game.resetState();
-
+  var game = new GameState();
+  $('.round-tracker').text(game.round);
+  $('.score-one').text('0');
+  $('.score-two').text('0');
+  $('.stack-one').text('0');
+  $('.stack-two').text('0');
+  $('.block-space').text('0/1');
+  
+  $('#start-game-button2').on('click', game.startGame);
+  $('#start-game-button3').on('click', game.startGame);
+  $('#start-game-button4').on('click', game.startGame);
 }
